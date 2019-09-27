2222a0650f21eb00ce20456b737c426b19a178f0
RudrThakur <rudrakshacmkt777@gmail.com>
2019-09-27 23:35:11 +0530

    fixed login bugs

--- leave2live/js/login.js
+++ leave2live/js/login.js
@@ -1,63 +1,74 @@
 ////////////////////////////////JS Handler for login.html
 
+//////////////
+// GLOBALS
+/////////////
+var loginFlag = false;
 
 ////////////////////////// Login Handler for Students
 
 var rootRef = firebase.database().ref("students");
 
-rootRef.once("child_added", snap =>{
-    var regnofromdb = snap.child("registernumber").val();
-    var passfromdb  = snap.child("password").val(); 
+rootRef.on("value", snap =>{
 
     //onclick handler for login-btn
-    $("#login-btn").click(function(){debugger;
+    $("#login-btn").click(function(){
         var regno = $("#registernumber").val();
         var pass = $("#pass").val();
 
-        //If credentials are Correct
-        if (regnofromdb == regno && passfromdb == pass){
+        snap.forEach(function(childNode){
+
+            var regnofromdb = childNode.child("registernumber").val();
+            var passfromdb  = childNode.child("password").val(); 
             
-        //alert success !
-        alert("Login Success");
-        
-        
-        //getting student profile from firebase
+            //If credentials are Correct
+            if (regnofromdb == regno && passfromdb == pass){
+                   
+                // Set Login Success Flag
+                loginFlag = true;
+                
+                //getting student profile from firebase
 
-        var studentName = snap.child("studentname").val();
-        var email  = snap.child("email").val(); 
-        var phone  = snap.child("phone").val(); 
-        var classAndSec = snap.child("classandsec").val();
-        var arrearCount = snap.child("arrearcount").val();
-        var department = snap.child("department").val();
-        var dob = snap.child("dob").val();
-        var leaveCount = snap.child("leavecount").val();
+                var studentName = snap.child("studentname").val();
+                var email  = snap.child("email").val(); 
+                var phone  = snap.child("phone").val(); 
+                var classAndSec = snap.child("classandsec").val();
+                var arrearCount = snap.child("arrearcount").val();
+                var department = snap.child("department").val();
+                var dob = snap.child("dob").val();
+                var leaveCount = snap.child("leavecount").val();
 
 
-        //using localstorage to pass them into Javascript pages
-        
-        localStorage.setItem("registernumber", regno);
-        localStorage.setItem("studentname", studentName);
-        localStorage.setItem("email", email);
-        localStorage.setItem("phone", phone);
-        localStorage.setItem("classandsec", classAndSec);
-        localStorage.setItem("arrearcount", arrearCount);
-        localStorage.setItem("department", department);
-        localStorage.setItem("dob", dob);
-        localStorage.setItem("leavecount", leaveCount);
-        
-        //redirect to index page
-        window.location.href ='index.html';
+                //using localstorage to pass them into Javascript pages
+                
+                localStorage.setItem("registernumber", regno);
+                localStorage.setItem("studentname", studentName);
+                localStorage.setItem("email", email);
+                localStorage.setItem("phone", phone);
+                localStorage.setItem("classandsec", classAndSec);
+                localStorage.setItem("arrearcount", arrearCount);
+                localStorage.setItem("department", department);
+                localStorage.setItem("dob", dob);
+                localStorage.setItem("leavecount", leaveCount);
+                
+            }
 
-        return false;
+           
+        });
+
+        if(loginFlag){
+            alert("Login Success !");
+            //redirect to index page
+            window.location.href ='index.html';
+            return false;
         }
 
-        //If Credentials are wrong
         else{
-        //alert Failure !
-        alert("Invalid Credentials !");
-
-        return true;
+            alert("Login Failed !");
+            return true;
         }
+    
+      
     });
 
 });
