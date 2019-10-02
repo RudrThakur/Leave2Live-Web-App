7ddd09eedb86049eb6080b6079c2018a3ebe44de
RudrThakur <rudrakshacmkt777@gmail.com>
2019-10-02 19:15:05 +0530

    basic dashboard for student leave requests

--- leave2live/hod/js/studentLeave.js
+++ leave2live/hod/js/studentLeave.js
@@ -1,3 +1,33 @@
+////////////////////////////////JS Handler for staffDashboard.html
+// Rudr Thakur
+
+///////////////////////////////////// GLOBALS /////////////////////////////////////////////
+
+//Global Row Indexes
+var rowInd = 0, rowIndInProcess = 0, rowIndCancelled = 0, rowIndHistory = 0;
+
+///////////  Session Handling ////////////////////
+
+// if (!localStorage.getItem("staffemail")) {
+//     window.location.href = '../login.html';
+
+// } 
+// else {
+   
+    //////////////////////////////////// Loading Student Profile ///////////////////////////
+
+
+    //Add values to Staff Profile Modal 
+    // $("#profile-staff-name").html(localStorage.getItem("staffname"));
+    // $("#profile-staff-id").html(localStorage.getItem("staffid"));
+    // $("#profile-staff-email").html(localStorage.getItem("staffemail"));
+    // $("#profile-staff-department").html(localStorage.getItem("staffdepartment"));
+    // $("#profile-staff-phone").html(localStorage.getItem("staffphone"));
+    // $("#profile-staff-isclassteacherof").html(localStorage.getItem("staffisclassteacherof"));
+    // $("#profile-staff-isacoof").html(localStorage.getItem("staffisacoof"));
+// }
+
+
 // Get the element with id="defaultOpen" and click on it
 
 document.getElementById("defaultOpen").click();
@@ -15,3 +45,192 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+studentLeaveRequestsRef = firebase.database().ref("requests");
+
+
+////////////////////////// NEW REQUESTS - STUDENTS ///////////////////////////////////
+studentLeaveRequestsRef.orderByChild("status").equalTo("submitted(HOD)").once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+    
+    var content = '';
+
+    //Retrieve Request Data
+    var tableData = child.val();
+
+    rowInd = rowInd + 1;
+
+    ////////////////////////////Get the Request Data of the Matched Record
+    var tableRequestId = child.key;
+    var tableRegisterNumber = tableData.registernumber;
+    var tableRequestType = tableData.requesttype;
+    var tableRequestDate = tableData.date;
+    var tableReasonCategory = tableData.reasoncategory;
+    var tableDayMode = tableData.daymode;
+    var tableStudentName;
+
+    ////////////////////////////////Join Requests table and Students table using Register Number
+
+    //reference to table - students
+    tableStudentsDataRef = firebase.database().ref("students");
+
+    tableStudentsDataRef.child(tableRegisterNumber).once("value", function(snap){
+
+            tableStudentName = snap.val().studentname;
+
+            //Append acquired data to table
+            ///////////////////////////////Display Request Data in Request-Table
+            content += '<tr>';
+            content += '<td>' + '<a href="staffDashboardCompleteRequestDetails.html?queryid=' + tableRequestId + '">' + tableRequestId + '</a>' + '</td>';//Column RequestID
+            content += '<td>' + tableRequestType + '</td>';//Column RequestType
+            content += '<td>' + tableRegisterNumber + '</td>';//Column RegisterNumber
+            content += '<td>' + tableStudentName + '</td>';//Column StudentName
+            content += '<td>' + tableRequestDate + '</td>'; //Column RequestDate
+            content += '<td>' + tableReasonCategory + '</td>';//Column Reason Category
+            content += '<td>' + tableDayMode + '</td>';//Column Day Mode
+            content += '</tr>';
+            $('#students-leave-table-new').append(content);
+     
+        });
+    });
+});
+
+////////////////////////// APPROVED REQUESTS - STUDENTS ///////////////////////////////////
+studentLeaveRequestsRef.orderByChild("status").equalTo("approved(HOD)").once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+    
+    var content = '';
+
+    //Retrieve Request Data
+    var tableData = child.val();
+
+    rowInd = rowInd + 1;
+
+    ////////////////////////////Get the Request Data of the Matched Record
+    var tableRequestId = child.key;
+    var tableRegisterNumber = tableData.registernumber;
+    var tableRequestType = tableData.requesttype;
+    var tableRequestDate = tableData.date;
+    var tableReasonCategory = tableData.reasoncategory;
+    var tableDayMode = tableData.daymode;
+    var tableStudentName;
+
+    ////////////////////////////////Join Requests table and Students table using Register Number
+
+    //reference to table - students
+    tableStudentsDataRef = firebase.database().ref("students");
+
+    tableStudentsDataRef.child(tableRegisterNumber).once("value", function(snap){
+
+            tableStudentName = snap.val().studentname;
+
+            //Append acquired data to table
+            ///////////////////////////////Display Request Data in Request-Table
+            content += '<tr>';
+            content += '<td>' + '<a href="staffDashboardCompleteRequestDetails.html?queryid=' + tableRequestId + '">' + tableRequestId + '</a>' + '</td>';//Column RequestID
+            content += '<td>' + tableRequestType + '</td>';//Column RequestType
+            content += '<td>' + tableRegisterNumber + '</td>';//Column RegisterNumber
+            content += '<td>' + tableStudentName + '</td>';//Column StudentName
+            content += '<td>' + tableRequestDate + '</td>'; //Column RequestDate
+            content += '<td>' + tableReasonCategory + '</td>';//Column Reason Category
+            content += '<td>' + tableDayMode + '</td>';//Column Day Mode
+            content += '</tr>';
+            $('#students-leave-table-approved').append(content);
+     
+        });
+    });
+});
+
+////////////////////////// CANCELLED REQUESTS - STUDENTS ///////////////////////////////////
+studentLeaveRequestsRef.orderByChild("status").equalTo("cancelled(HOD)").once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+    
+    var content = '';
+
+    //Retrieve Request Data
+    var tableData = child.val();
+
+    rowInd = rowInd + 1;
+
+    ////////////////////////////Get the Request Data of the Matched Record
+    var tableRequestId = child.key;
+    var tableRegisterNumber = tableData.registernumber;
+    var tableRequestType = tableData.requesttype;
+    var tableRequestDate = tableData.date;
+    var tableReasonCategory = tableData.reasoncategory;
+    var tableDayMode = tableData.daymode;
+    var tableStudentName;
+
+    if(tableRequestType == "Leave"){
+        ////////////////////////////////Join Requests table and Students table using Register Number
+
+        //reference to table - students
+        tableStudentsDataRef = firebase.database().ref("students");
+
+        tableStudentsDataRef.child(tableRegisterNumber).once("value", function(snap){
+
+            tableStudentName = snap.val().studentname;
+
+            //Append acquired data to table
+            ///////////////////////////////Display Request Data in Request-Table
+            content += '<tr>';
+            content += '<td>' + '<a href="staffDashboardCompleteRequestDetails.html?queryid=' + tableRequestId + '">' + tableRequestId + '</a>' + '</td>';//Column RequestID
+            content += '<td>' + tableRequestType + '</td>';//Column RequestType
+            content += '<td>' + tableRegisterNumber + '</td>';//Column RegisterNumber
+            content += '<td>' + tableStudentName + '</td>';//Column StudentName
+            content += '<td>' + tableRequestDate + '</td>'; //Column RequestDate
+            content += '<td>' + tableReasonCategory + '</td>';//Column Reason Category
+            content += '<td>' + tableDayMode + '</td>';//Column Day Mode
+            content += '</tr>';
+            $('#students-leave-table-cancelled').append(content);
+            
+            });
+        }
+    });
+});
+
+////////////////////////// HISTORY REQUESTS - STUDENTS ///////////////////////////////////
+studentLeaveRequestsRef.once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+    
+    var content = '';
+
+    //Retrieve Request Data
+    var tableData = child.val();
+
+    rowInd = rowInd + 1;
+
+    ////////////////////////////Get the Request Data of the Matched Record
+    var tableRequestId = child.key;
+    var tableRegisterNumber = tableData.registernumber;
+    var tableRequestType = tableData.requesttype;
+    var tableRequestDate = tableData.date;
+    var tableReasonCategory = tableData.reasoncategory;
+    var tableDayMode = tableData.daymode;
+    var tableStudentName;
+
+    ////////////////////////////////Join Requests table and Students table using Register Number
+
+    //reference to table - students
+    tableStudentsDataRef = firebase.database().ref("students");
+
+    tableStudentsDataRef.child(tableRegisterNumber).once("value", function(snap){
+
+            tableStudentName = snap.val().studentname;
+
+            //Append acquired data to table
+            ///////////////////////////////Display Request Data in Request-Table
+            content += '<tr>';
+            content += '<td>' + '<a href="staffDashboardCompleteRequestDetails.html?queryid=' + tableRequestId + '">' + tableRequestId + '</a>' + '</td>';//Column RequestID
+            content += '<td>' + tableRequestType + '</td>';//Column RequestType
+            content += '<td>' + tableRegisterNumber + '</td>';//Column RegisterNumber
+            content += '<td>' + tableStudentName + '</td>';//Column StudentName
+            content += '<td>' + tableRequestDate + '</td>'; //Column RequestDate
+            content += '<td>' + tableReasonCategory + '</td>';//Column Reason Category
+            content += '<td>' + tableDayMode + '</td>';//Column Day Mode
+            content += '</tr>';
+            $('#students-leave-table-history').append(content);
+     
+        });
+    });
+});
