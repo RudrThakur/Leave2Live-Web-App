5b0f2dc9d816a632b419308af11768b46a5bdc6a
RudrThakur <rudrakshacmkt777@gmail.com>
2019-10-07 21:28:55 +0530

    Added attach proof functionality for students dashboard

--- leave2live/js/requestDetails.js
+++ leave2live/js/requestDetails.js
@@ -1,6 +1,11 @@
 //////////////// JS Handler for requestDetails.html
 // Rudr Thakur
 
+
+////////////////////////////////////////////// GLOBALS /////////////////////////////////////////////
+var selectedFile;
+var currentRequestId;
+
 ////////////////////////////////////////////// Functions ///////////////////////////////////////////
 
 //function to reverse a date
@@ -54,6 +59,7 @@ detailRequestRef.orderByKey().equalTo(queryRequestId).on("value", function(snaps
         //Retrieve Request Data
         var detailRequestData = child.val();
 
+        currentRequestId = child.key;
 
         ///////////////////////////Display Data in the Request Details Modal
         $("#requestid-details").html(child.key);
@@ -70,10 +76,65 @@ detailRequestRef.orderByKey().equalTo(queryRequestId).on("value", function(snaps
         $("#attendance-details").html(detailRequestData.attendance);
         $("#leave-count-details").html(detailRequestData.leavecount);
         $("#status-details").html(detailRequestData.status);
+        $("#classteacher-remarks-details").html(detailRequestData.classteacherremarks);
+        $("#aco-remarks-details").html(detailRequestData.acoremarks);
+        $("#hod-remarks-details").html(detailRequestData.hodremarks);
  
     });
 });
  
+//////////////////////////////////////
+// Proof Attachment Handler
+/////////////////////////////////////
+
+$("#proof-file").on("change", function(event){
+
+    selectedFile = event.target.files[0];
+    $("#btn-proof-upload").show();
+});
+
+function uploadProof(){
+    var fileName = selectedFile.name;
+    // Create a root reference
+    var storageRef = firebase.storage().ref("/studentProofs/" + fileName);
+    var uploadTask = storageRef.put(selectedFile);
+
+    uploadTask.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+        alert(error);
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          console.log('File available at', downloadURL);
+
+          var proofKey = firebase.database().ref("studentproofs/").push().key;
+          var proofUpdates = {};
+          var proofData = {
+              url : downloadURL,
+              requestid: currentRequestId
+          };
+
+          proofUpdates["/studentproofs/" + proofKey] = proofData;
+          firebase.database().ref().update(proofUpdates);
+        });
+      });
+
+}
+
 
 //When back button is Clicked
 $("#back-btn").click(function(){
